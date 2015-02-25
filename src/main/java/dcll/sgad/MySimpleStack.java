0550ed2b493fdcab5f657f1c198c0625893f3037
Sebastien Gadois <sgadois@gmail.com>
2015-02-25 14:35:50 +0100

    Add MySimpleStack implementation with coverage tests

---
+++ src/main/java/dcll/sgad/MySimpleStack.java
@@ -0,0 +1,41 @@
+package dcll.sgad;
+
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+
+/**
+ * Created by seb on 25/02/15.
+ */
+public class MySimpleStack implements SimpleStack {
+
+    ArrayList<Item> stack = new ArrayList<Item>();
+
+    @Override
+    public boolean isEmpty() {
+        if (stack.isEmpty())
+            return true;
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return stack.size();
+    }
+
+    @Override
+    public void push(Item item) {
+        stack.add(item);
+    }
+
+    @Override
+    public Item peek() throws EmptyStackException {
+        return stack.get(stack.size() - 1);
+    }
+
+    @Override
+    public Item pop() throws EmptyStackException {
+        Item item = stack.get(stack.size() - 1);
+        stack.remove(stack.size() - 1);
+        return item;
+    }
+}
---
+++ src/test/java/dcll/sgad/SimpleStackTest.java
@@ -0,0 +1,67 @@
+package dcll.sgad;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.EmptyStackException;
+
+/**
+ * Created by seb on 25/02/15.
+ */
+public class SimpleStackTest {
+
+    @Test
+    public void testCreateEmptyStack() { // Test case
+
+        // Code under test
+        SimpleStack stack = new MySimpleStack();
+
+        // Assertions (oracle)
+        Assert.assertTrue("A new stack should be empty", stack.isEmpty());
+        Assert.assertEquals("A new stack has no element", 0, stack.getSize());
+    }
+
+    @Test
+    public void testPush() throws EmptyStackException {
+
+        // Setup the "state of the world"
+        SimpleStack stack = new MySimpleStack();
+        Item item = new Item();
+
+        // Code under test
+        stack.push(item);
+
+        // assertions (oracle)
+        Assert.assertFalse("The stack must be not empty", stack.isEmpty());
+        Assert.assertEquals("The stack constains 1 item", 1, stack.getSize());
+        Assert.assertSame("The pushed item is on top of the stack", item, stack.peek());
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void testPopOnEmptyStack()  throws EmptyStackException {
+        // Setup the "state of the world"
+        SimpleStack stack = new MySimpleStack();
+
+        // Code under test
+        stack.pop(); // should throws an EmptyStackException.
+    }
+
+    @Test
+    public void testPop() throws EmptyStackException {
+        // Setup the "state of the world"
+        SimpleStack stack = new MySimpleStack();
+        Item item = new Item();
+        Item itemRetrieve;
+        stack.push(item);
+
+        // Code under test
+        itemRetrieve = stack.pop();
+
+        // assertions (oracle)
+        Assert.assertTrue("The stack must be empty", stack.isEmpty());
+        Assert.assertEquals("The stack constains 0 item", 0, stack.getSize());
+        Assert.assertEquals("The item retrieve is the one on top of the stack", item, itemRetrieve);
+    }
+
+
+}
